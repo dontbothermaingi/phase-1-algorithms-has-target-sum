b4f54b26d5e0edfb95fc816e4aad794645dc59a6
+ <+>
2024-01-10 14:57:53 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+  // create a new set where the numbers iterated through will be kept tracked of
+  const seenNumbers = new Set();
+
+  // iterate through the the array that contains the numbers 
+  for(let num of array){
+    if(seenNumbers.has(target-num)){
+      return true;
+    }
+    // Otherwise, add the current number to the set
+   seenNumbers.add(num);
+  }
+
+   // No pair found, return false
+  return false;
 }
 
 /* 
@@ -8,10 +23,18 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  1.Create an empty set that will keep track of the numbers in the array
+  2.Iterate through the array looking for a pair that when added will make the target number
+  3.Subtract num from the target and the answer gotten to be searched for in the new set.
+  4.If the num is found return true else return false
 */
 
 /*
   Add written explanation of your solution here
+  The function first creates a new set where it will keep track of the numbers that have been iterated through
+  It will then iterate through the array subtarcting each number from the target.
+  The difference between the num and the target will be looked for in the new set.
+  If found in the new set return true else return false
 */
 
 // You can run `node index.js` to view these console logs
